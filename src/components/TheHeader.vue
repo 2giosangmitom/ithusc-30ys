a4a4f3aea5c1fc173b2ee2df0774c078f0cb7520
Vo Quang chien <voquangchien.dev@proton.me>
2024-11-07 09:01:07 +0700

    feat(header): add close button when sidebar be opened

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { Icon } from "@iconify/vue";
 
 // Navigation parts
@@ -11,9 +11,6 @@ const parts = [
 ];
 
 const isOpen = ref(false); // Mobile sidebar state
-const triggerIcon = computed(() =>
-  isOpen.value ? "mingcute:close-fill" : "mingcute:menu-fill"
-);
 </script>
 
 <template>
@@ -24,10 +21,13 @@ const triggerIcon = computed(() =>
 
     <nav class="mobile_nav">
       <button class="mobile_nav__trigger" @click="isOpen = !isOpen">
-        <Icon :icon="triggerIcon" />
+        <Icon icon="mingcute:menu-fill" />
       </button>
 
       <ul class="mobile_nav__list" :class="{ opened: isOpen }">
+        <button class="mobile_nav__list__close_btn" @click="isOpen = !isOpen">
+          <Icon icon="mingcute:close-fill" />
+        </button>
         <li>
           <img src="/images/logo.png" alt="logo" />
         </li>
@@ -75,7 +75,7 @@ const triggerIcon = computed(() =>
   justify-content: center;
   outline: none;
   position: relative;
-  z-index: 10;
+  z-index: 0;
   border: 2px solid var(--orange);
   padding: 3px;
   border-radius: 5px;
@@ -110,6 +110,30 @@ const triggerIcon = computed(() =>
   z-index: 5;
 }
 
+.mobile_nav__list__close_btn {
+  background-color: transparent;
+  color: var(--text);
+  font-size: 2rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  outline: none;
+  position: absolute;
+  z-index: 0;
+  border: 2px solid var(--orange);
+  padding: 3px;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: all 0.3s;
+  left: 0;
+  margin-left: 30px;
+}
+
+.mobile_nav__list__close_btn:hover {
+  background-color: var(--orange);
+  color: var(--accent-color);
+}
+
 .mobile_nav__list.opened {
   transform: translateX(0);
 }
@@ -120,15 +144,15 @@ const triggerIcon = computed(() =>
   transition: all 0.3s;
 }
 
-.mobile_nav__list li:not(:first-child) a:hover {
+.mobile_nav__list li:not(:first-child :nth-child(2)) a:hover {
   color: var(--orange);
 }
 
-.mobile_nav__list li:first-child {
+.mobile_nav__list:nth-child(2) {
   margin-bottom: 30px;
 }
 
-.mobile_nav__list li:first-child img {
+.mobile_nav__list:nth-child(2) img {
   width: 50px;
   height: 50px;
 }
