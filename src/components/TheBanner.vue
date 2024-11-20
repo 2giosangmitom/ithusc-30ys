ee1abce225d143fa29d6c389dd88a70a16993c2f
Vo Quang chien <voquangchien.dev@proton.me>
2024-11-20 20:34:09 +0700

    feat: update banner ui

--- src/components/TheBanner.vue
+++ src/components/TheBanner.vue
@@ -1,46 +1,19 @@
 <template>
   <section id="home">
-    <h1>30th Anniversary</h1>
-    <div class="home__vertical_line"></div>
-    <h2>Kỉ niệm 30 năm thành lập khoa Công nghệ thông tin</h2>
-    <div class="home__horizontal_line"></div>
-    <img src="/images/30th_anniversary_banner.jpg" alt="banner" />
+    <img src="/images/30th_anniversary_banner.jpg" alt="30th Anniversary Banner" class="home__banner" />
   </section>
 </template>
 
 <style scoped>
 #home {
   text-align: center;
-  margin-top: 2rem;
-}
-
-#home h1 {
-  font-family: "Playfair Display", sans-serif;
-  font-size: 2.5rem;
-  font-weight: 500;
-}
-
-#home h2 {
-  font-weight: 500;
-  font-family: "Playfair Display", sans-serif;
+  margin: 2rem auto;
 }
 
 #home img {
   width: 100%;
-}
-
-.home__vertical_line {
-  height: 100px;
-  width: 1px;
-  margin: 10px auto;
-  background-color: grey;
-  opacity: 0.5;
-}
-
-.home__horizontal_line {
-  width: 50%;
-  height: 2px;
-  background-color: grey;
-  margin: 2rem auto;
+  max-width: 800px;
+  border-radius: 10px;
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
 }
 </style>
