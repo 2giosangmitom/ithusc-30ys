3ba47b7cb9f78483341b2bbd587e4e8e10213a8a
Vo Quang chien <voquangchien.dev@proton.me>
2024-11-21 16:37:16 +0700

    feat: implement mobile navigation in header and update styles

--- uno.config.ts
+++ uno.config.ts
@@ -3,4 +3,11 @@ import { presetUno } from "unocss";
 
 export default defineConfig({
   presets: [presetUno()],
+  rules: [
+    [
+      /^border-radius-([\.\d]+)(px|em|rem|%)?$/,
+      ([_, num, unit = "px"]) => ({ "border-radius": `${num}${unit}` }),
+    ],
+    ["bg-accent", { "background-color": "#26335d" }],
+  ],
 });
